9945c17221666adf1d90102f69ca13b4ce4bbf67
raedaltaki <raed.simon@gmail.com>
2020-12-27 19:27:57 -0500

    style button update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 var resumeEl = document.getElementById("resume");
+var timeoutStop;
+var intervalStop;
 
 var downloadResume = function ()
 {
@@ -9,16 +11,27 @@ var downloadResume = function ()
 
 var colorChange = function ()
 {
-    resumeEl.style.background = "grey";
+    resumeEl.style.background = 'grey';
     resumeEl.style.color = 'white';
-    setTimeout("resumeEl.style.background = 'white'; resumeEl.style.color = 'black';",500);
+    timeoutStop = setTimeout("resumeEl.style.background = 'white'; resumeEl.style.color = 'black';",500);
 }
 
-// var ColorWhite = function ()
-// {
-    
-// }
+var buttonMouseOver = function()
+{
+    clearTimeout(timeoutStop);
+    clearInterval(intervalStop);
+    resumeEl.style.background = 'grey';
+    resumeEl.style.color = 'white';
+}
 
-setInterval(colorChange,1000);
+var buttonMouseLeave = function()
+{
+    intervalStop = setInterval(colorChange,1000);
+    timeoutStop = setTimeout("resumeEl.style.background = 'white'; resumeEl.style.color = 'black';",500);
+}
+
+intervalStop = setInterval(colorChange,1000);
 
 resumeEl.addEventListener("click",downloadResume);
+resumeEl.addEventListener("mouseover", buttonMouseOver);
+resumeEl.addEventListener("mouseleave",buttonMouseLeave);
