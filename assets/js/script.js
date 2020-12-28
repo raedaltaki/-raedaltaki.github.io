e6de8b3c14125ebe72bdc1d8e65cb14ce6d2f187
raedaltaki <raed.simon@gmail.com>
2020-12-27 19:02:04 -0500

    style resume button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,21 @@ var downloadResume = function ()
 {
     window.open(".\\assets\\resume\\Resume_Raed Altaki_Web Developer.pdf");
     // resumeEl.setAttribute("href", "..\\resume\\Resume_Raed Altaki_BELL_Software Developer.pdf");
-    // resumeEl.setAttribute("download", filename);
+    // resumeEl.setAttribute("download", "..\\resume\\Resume_Raed Altaki_BELL_Software Developer.pdf");
 }
+
+var colorChange = function ()
+{
+    resumeEl.style.background = "grey";
+    resumeEl.style.color = 'white';
+    setTimeout("resumeEl.style.background = 'white'; resumeEl.style.color = 'black';",500);
+}
+
+// var ColorWhite = function ()
+// {
+    
+// }
+
+setInterval(colorChange,1000);
 
 resumeEl.addEventListener("click",downloadResume);
